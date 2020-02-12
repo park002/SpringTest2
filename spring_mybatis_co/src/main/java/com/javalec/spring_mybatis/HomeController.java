e7be2b223f79ba2707906f75e9f819a6e22e81b9
pjh <ekem159@naver.com>
2020-02-12 18:40:50 +0900

    Spring Mybatis(3/3)

--- spring_mybatis_co/src/main/java/com/javalec/spring_mybatis/HomeController.java
+++ spring_mybatis_co/src/main/java/com/javalec/spring_mybatis/HomeController.java
@@ -1,12 +1,12 @@
 package com.javalec.spring_mybatis;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javalec.spring_mybatis.dao.ContentDao;
-import com.javalec.spring_mybatis.dto.ContentDto;
+import com.javalec.spring_mybatis.dao.IDao;
 
 /**
  * Handles requests for the application home page.
@@ -27,6 +27,8 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	ContentDao dao;
+	@Autowired
+	private SqlSession sqlSession;
 
 	@Autowired
 	public void setDao(ContentDao dao) {
@@ -52,8 +54,11 @@ public class HomeController {
 
 	@RequestMapping("/list")
 	public String list(Model model) {
-		ArrayList<ContentDto> dtos = dao.listDao();
-		model.addAttribute("list", dtos);
+		// ArrayList<ContentDto> dtos = dao.listDao();
+		IDao dao = sqlSession.getMapper(IDao.class);
+		// model.addAttribute("list", dtos);
+		model.addAttribute("list", dao.listDao());
+
 		return "/list";
 	}
 
@@ -65,18 +70,19 @@ public class HomeController {
 	// mWriter mContent
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
 	public String write(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
 		dao.writeDao(request.getParameter("mWriter"), request.getParameter("mContent"));
 		return "redirect:list";
 	}
 
 	@RequestMapping("/view")
 	public String view() {
-
 		return "/view";
 	}
 
 	@RequestMapping("/delete")
 	public String delete(HttpServletRequest request, Model model) {
+		IDao dao = sqlSession.getMapper(IDao.class);
 		dao.deleteDao(request.getParameter("mId"));
 		return "redirect:list";
 	}
